3d037ecf31ac1019788f5ca2eb5b56cbd4ed1533
Valmiki <valmiki.kothare.vk@gmail.com>
2024-08-21 20:25:05 -0400

    Tested all scripts

--- jupyter.sh
+++ jupyter.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/bash
 #SBATCH -J jupyter
 #SBATCH -t 08:00:00
-#SBATCH --gres=gpu:a100:1
 #SBATCH -N 1
-#SBATCH -c 4
-#SBATCH --constraint=rocky8
-#SBATCH --mem 20G
+#SBATCH -c 1
+#SBATCH --mem 1G
 #SBATCH -o jupyter.out
 
 source ~/.bashrc
---
+++ om_config.sh
@@ -0,0 +1,22 @@
+# Description: Configuration file for OpenMind scripts
+
+# Set the directory where all scripts are located
+cd "$(dirname "$0")"
+# Set the user_id
+export user_id="valmiki"
+# Name the sbatch script for vscode
+export vscode_script="vscode.sh"
+# Name the sbatch script for jupyter
+export jupyter_script="jupyter.sh"
+# Name the sbatch jupyter output file
+export jupyter_out="jupyter.out"
+# Local directory
+export local_script_dir="$(pwd)"
+# Remote directory
+export remote_script_dir="/om2/user/$user_id/slurm_scripts"
+# Node wait timeout 
+export timeout_limit=120
+# Define port used to login to JupyterLab on local computer
+export local_port=$(($RANDOM % (65535 - 49152) + 49152))
+# Enter port used by JupyterLab container running on OpenMind
+export remote_port=$(($RANDOM % (65535 - 49152) + 49152))
