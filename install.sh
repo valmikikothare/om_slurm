5328d354eeb6e8743769614acc14028d951b9a8b
Valmiki Kothare <valmiki.kothare.vk@gmail.com>
2024-11-05 16:34:37 -0500

    Modified install.sh for mac

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@ root=$(dirname $(readlink -f $0))
 
 for file in om_jupyter om_cancel om_rename_node om_ssh om_vscode; do
     chmod +x "$root/$file"
-    sudo ln -sf "$root/$file" "/usr/local/bin/$file"
 done
+
+echo "PATH=\$PATH:$root" >> ~/.bashrc
