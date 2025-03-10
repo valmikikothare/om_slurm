3314118326bcc437cd43f03124b2e7627ff57f5d
Valmiki Kothare <valmiki@mit.edu>
2025-03-10 12:03:34 -0400

    Added om_queue command

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ script_dir=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
 echo "Installing om_server scripts to $HOME/.local/bin"
 
-for file in om_jupyter om_cancel om_rename_node om_ssh om_vscode; do
+for file in om_jupyter om_cancel om_rename_node om_ssh om_vscode om_queue; do
     chmod +x "$script_dir/$file"
 done
 
@@ -18,6 +18,7 @@ ln -sf $script_dir/om_vscode $HOME/.local/bin/om_vscode
 ln -sf $script_dir/om_rename_node $HOME/.local/bin/om_rename_node
 ln -sf $script_dir/om_ssh $HOME/.local/bin/om_ssh
 ln -sf $script_dir/om_cancel $HOME/.local/bin/om_cancel
+ln -sf $script_dir/om_queue $HOME/.local/bin/om_queue
 
 # Add the .local/bin directory to the PATH
 # Check if $HOME/.local/bin is already in the PATH
@@ -26,4 +27,5 @@ if ! echo $PATH | grep -q "$HOME/.local/bin"; then
     echo "export PATH=\"$HOME/.local/bin:\$PATH\"" >> ~/.bashrc
 fi
 
-echo "Installation complete. You can now use the om_jupyter, om_vscode, om_rename_node, om_ssh, and om_cancel commands."
+echo "Installation complete. You can now use the om_jupyter, om_vscode,"\
+     "om_rename_node, om_ssh, om_cancel, and om_queue commands."
